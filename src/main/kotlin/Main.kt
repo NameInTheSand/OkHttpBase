0921ded341d18c9e685d26c517175fda688f1eb7
Sam <semernnaduev@gmail.com>
2024-03-17 21:54:04 +0100

    Added a initial serialization/deserializatio

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,88 +1,43 @@
 package org.example
 
 import com.google.gson.Gson
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.logging.HttpLoggingInterceptor
-import org.example.data.AnimeEntity
-import org.example.network.HttpRoutes
-import org.example.network.NetworkException
-import org.example.network.PageNotFoundException
+import com.google.gson.annotations.Expose
 
-private val contentType = "application/json; charset=utf-8".toMediaType()
+data class Author(
+    val name: String,
+    val age: Int,
+    val books: List<Book>,
+    val isAlive:Boolean,
+    @Expose
+    val ignored:String = "Ignored"
+)
+
+data class Book(
+    val name: String,
+    val year: Int
+)
 
 fun main() {
     val gson = Gson()
-    val loggingInterceptor = HttpLoggingInterceptor()
-        .setLevel(HttpLoggingInterceptor.Level.BODY)
-
-    val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(loggingInterceptor)
-        .build()
-
-    val getRequest = Request.Builder()
-        .get()
-        .url(HttpRoutes.getList)
-        .build()
-
-    val getCall = okHttpClient.newCall(getRequest)
-
-    val postBody = gson.toJson(
-        AnimeEntity(
-            id = "123",
-            name = "Name",
-            creationTime = System.currentTimeMillis(),
-            review = "Review",
-            genre = "Genre",
-            year = 1999,
-            rating = 7,
-            posterLink = "Custom link"
-        )
-    ).toRequestBody(contentType)
-
-    val postRequest = Request.Builder()
-        .post(postBody)
-        .url(HttpRoutes.postAnime)
-        .build()
-
-    val postCall = okHttpClient.newCall(postRequest)
-
-    try {
-        val request = postCall.execute()
-        if (request.isSuccessful) {
-            println(request.code)
-        } else {
-            if (request.code == 404) {
-                throw PageNotFoundException()
-            }
-            throw IllegalStateException("Error")
+    val jsonString = """
+        {
+            "name":"John",
+            "age":42,
+            "books":[
+                {"name":"Book 1", year:1999}, {"name":"Book 2", year:1989},{"name":"Book 3", year:1969}
+            ],
+            "isAlive":true
         }
-    } catch (e: Exception) {
-        if (e is NetworkException) {
-            println("Network message " + e.message)
-        } else {
-            println(e.message)
-        }
-    }
+    """.trimIndent()
 
-    try {
-        val request = getCall.execute()
-        if (request.isSuccessful) {
-            println(request.code)
-        } else {
-            if (request.code == 404) {
-                throw PageNotFoundException()
-            }
-            throw IllegalStateException("Error")
-        }
-    } catch (e: Exception) {
-        if (e is NetworkException) {
-            println("Network message " + e.message)
-        } else {
-            println(e.message)
-        }
+    val author = gson.fromJson(jsonString, Author::class.java)
+
+    println(author.name)
+    println(author.age)
+    println(author.isAlive)
+    println(author.ignored)
+    author.books.forEach {
+        println(it)
     }
 
 }
